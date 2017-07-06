c55a5683609f2029993f272bf6b9307f02ac5a6e
Bill <networker747@gmail.com>
2017-07-06 22:45:05 +0000

    added in New Categories input

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,9 @@
 class CategoriesController < ApplicationController
 
+
   def show
     @category = Category.find(params[:id])
+    @categories = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
 

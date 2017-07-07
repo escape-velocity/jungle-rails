062360f881ef296193b5ae9a1ffa1938f66423a0
Bill <networker747@gmail.com>
2017-07-07 23:23:12 +0000

    fixed HTTP auth security for Admin pages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,9 @@
 class UserMailer < ApplicationMailer
 default from: 'no-reply@jungle.com'
  
-  def welcome_email(user)
+  def receipt_email(user)
     @user = user
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Thanks for your purchase')
   end
-end
-
 end

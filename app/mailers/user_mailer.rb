1423eee59b46be5efb2c591131e18e94e1cf2640
Bill <networker747@gmail.com>
2017-07-08 20:18:32 +0000

    fixed buggy Pay button and admin auth issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
 default from: 'no-reply@jungle.com'
  
-  def receipt_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Thanks for your purchase')
+  def receipt_email(order)
+    @order = order
+    @order_product = @order.line_items
+    mail(to: @order.email, subject:"Your order with Jungle: Order ##{@order.id}")
   end
 end
